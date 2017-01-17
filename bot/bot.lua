1b8e6e440efe1c6b77a6285a88469f866e000446
mohamaddarya <mohamaddarya80@gmail.com>
2017-01-17 23:57:22 +0330

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -228,7 +228,7 @@ function create_config( )
       "weather",
       "xkcd",
       "youtube" },
-    sudo_users = {168398326},
+    sudo_users = {mAmAd_TANHA_SKY_BLACKVIN},
     disabled_channels = {}
   }
   serialize_to_file(config, './data/config.lua')
